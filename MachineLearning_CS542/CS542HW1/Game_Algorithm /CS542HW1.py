16b9a8909f30ea27a1a9ce792cb39f0e1b847167
paulngouchet <ngouchet@bu.edu>
2020-09-01 01:47:55 -0400

    Clean Code

--- MachineLearning_CS542/CS542HW1/Game_Algorithm /CS542HW1.py
+++ MachineLearning_CS542/CS542HW1/Game_Algorithm /CS542HW1.py
@@ -1,10 +1,7 @@
 import numpy as np
 
-
 #formatting data to be able to use them properly
-
 reading = open('game.txt')
-
 previous = []
 prediction = []
 newPlay = []
@@ -17,16 +14,12 @@ def read2(f):
             line2 = ''
 
         yield line, line2
-
-
-
-
+	
 for line, line2 in read2(reading):
     data = line.split()
     previous.append(data)
     data = line2.split()
     prediction.append(data)
-
 
 reading = open('newTurn.txt')
 
@@ -34,15 +27,9 @@ for line in reading:
     data = line.split()
     newPlay = data
 
-
-
-
-
 x = np.array(previous, dtype = float)
 y = np.array(prediction, dtype = float)
 play = np.array(newPlay, dtype = float)
-
-
 
 class Neural_Network(object):
 	def __init__(self):
@@ -55,12 +42,11 @@ class Neural_Network(object):
 		self.w2 = np.random.randn(self.hiddenLayerSize, self.outputLayerSize)
 
 	def forward(self, x):
-	#propagate inputs through network
+		#propagate inputs through network
 		self.z2 = np.dot(x, self.w1)
 		self.a2 = self.sigmoid(self.z2)
 		self.z3 = np.dot(self.a2, self.w2)
 		yHat = self.sigmoid(self.z3)
-
 		return yHat
 
 	def sigmoid(self, z):
@@ -72,7 +58,6 @@ class Neural_Network(object):
 
 	def costFunction(self, y , yHat):
 		cost = sum(0.5*(y-yHat)**2)
-
 		return cost
 
 	def costFunctionPrime(self, x, y):
@@ -80,40 +65,24 @@ class Neural_Network(object):
 		self.yHat = self.forward(x)
 		delta3 = np.multiply(-(y - self.yHat), self.sigmoidPrime(self.z3))
 		dJdW2 = np.dot(self.a2.T, delta3)
-
 		delta2 = np.dot(delta3, self.w2.T)*self.sigmoidPrime(self.z2)
 		dJdW1 = np.dot(x.T, delta2)
-
 		return dJdW1, dJdW2
 
-
 pattern = Neural_Network()
-
 print(" Rock = 0.33,  Paper = 0.66, Scissor = 1 ")
 print("the initial data are ")
 print(y)
 
-
-
 for _ in range(100000):
-
 	dJdW1, dJdW2 = pattern.costFunctionPrime(x,y)
 	learningRate = 1
 	pattern.w1 = pattern.w1 - learningRate*dJdW1
 	pattern.w2 = pattern.w2 - learningRate*dJdW2
 
-
-
 print("the new trained predicted result is ")
-
 print(pattern.forward(x))
-
 print("the possible value of the computer move in the next round is ")
 print(pattern.forward(play))
-
 print("This is the possible value of the next move of the computer, play the winning move based on that")
-
-
-
-
 #print( pattern.forward(previousPlay))
